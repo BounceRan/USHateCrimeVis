0d43ed2ceb5465f32cb190434ed76c8b53fdf967
Dan Ran <randanfx@gmail.com>
2018-12-11 21:03:52 -0500

    vis update

--- hatecrimevis.js
+++ hatecrimevis.js
@@ -15,37 +15,60 @@ var motivationArray3 = [],
   targetArray3 = [],
   incidentsArray3 = [];
 var condition = 0;
+
+var heightVis = 500;
+
+var allignDataset=incidentsArray3;
+var allignDatasetforCondition2=[];
 // function preload() {
 //     // load the CSV data into our `table` variable and clip out the header row
 //     table17 = loadTable("dataset/2017HCbyBiasMotivation.csv", "csv", "header");
 //     table16 = loadTable("dataset/2016HCbyBiasMotivation.csv", "csv", "header");
 // }
+
+
 var svg = d3.selectAll("#viz") //.append("div").attr("class", "column")
   .append("svg")
   .attr("id", "firstData")
-  .attr("height", 1600)
-  .attr("width", "320")
+  .attr("height", heightVis)
+
+  //.attr("min-width",'300')
+  //.attr("width", '33.3%')
+  .attr("width", '380')
+  //.style("display", "inline")
   //  .attr("x",0)
   .style("left", '0')
-  .style('background', 'lightgrey');
+//.style('background', 'lightgrey');
 //.attr("viewBox", "0 0 300 300");
 
 var svg2 = d3.selectAll("#viz") //.append("div").attr("class", "column")
   .append("svg")
   .attr("id", "secondData")
-  .attr("height", 1600)
-  .attr("width", "320")
-  .style("left", "320")
-  .style('background', 'lightgrey');
+  .attr("height", heightVis)
+  //.attr("width", "33.3%")
+  //.attr("min-width",'300')
+  //.style("left", "33.3%")
+  //.style("display", "inline")
+  .attr("width", '380')
+  .style("left", "380")
+
+//.style('background', 'lightgrey');
 
 var svg3 = d3.selectAll("#viz") //.append("div").attr("class", "column")
   .append("svg")
   .attr("id", "thirdData")
-  .attr("height", 1600)
-  .attr("width", "400")
-  .style("left", '640')
-  //.style("position","absolute")
-  .style('background', 'lightgrey');
+  .attr("height", heightVis)
+  //.attr("width", "33.3%")
+  //.attr("min-width",'300')
+  //.style("left", '66.6%')
+  .attr("width", '380')
+  .style("left", "760")
+
+//.style("position","absolute")
+//.style('background', 'lightgrey');
+
+
+
 
 d3.csv("dataset/2015HCbyBiasMotivation.csv", function(error, data) {
   if (error) throw error;
@@ -81,12 +104,12 @@ d3.csv("dataset/2015HCbyBiasMotivation.csv", function(error, data) {
       incidentsArray3.push(Number(data[i].incidents));
     }
 
-
-    conditionOne(incidentsArray, 130, svg);
-
-    conditionOne(incidentsArray2, 130, svg2);
     conditionOne(incidentsArray3, 130, svg3);
 
+
+    conditionOne(incidentsArray2, 130, svg2);
+
+      conditionOne(incidentsArray, 130, svg);
 
   });
 
@@ -94,7 +117,6 @@ d3.csv("dataset/2015HCbyBiasMotivation.csv", function(error, data) {
 
 
 });
-
 
 
 
@@ -117,14 +139,14 @@ function conditionOne(inArray, x1, svgPic) {
   var reSumOfInci = d3.scaleLinear()
     .domain([5600, 7500])
     .range([80, 120]);
+  // .range([window.innerWidth*.33*.266,window.innerWidth*.33*.4]);
   console.log(reSumOfInci(sumOfInci1));
   svgPic.selectAll("g").remove();
   svgPic.selectAll("circle").remove();
   svgPic.selectAll("text").remove();
+  d3.selectAll("svg").attr("height", heightVis);
 
   var circles = svgPic //.selectAll("circle")
-  // .data([0,1,2])
-  // .enter()
 
   // first data set
   circles.append("g").append("circle")
@@ -161,18 +183,30 @@ function conditionOne(inArray, x1, svgPic) {
       return x1
     });
 
-}
+
+
+};
+
+
+
+
+
 
 
 //*********************************************
 function conditionTwo(inArray, moArray, svgPic) {
+
   svgPic.selectAll("g").remove();
   svgPic.selectAll("text").remove();
   svgPic.selectAll("circle").remove();
+
+  d3.selectAll("svg").attr("height", heightVis);
+
+
   let xP = 130;
 
-   this.inArray=inArray;
-   this.svgPic=svgPic;
+  this.inArray = inArray;
+  this.svgPic = svgPic;
 
   var ethni = [],
     reli = [],
@@ -203,15 +237,22 @@ function conditionTwo(inArray, moArray, svgPic) {
     //console.log(a+"sssssssssss");
   };
 
+
+
   var reIncidentsArray = d3.scaleLinear()
     .domain([0, 4200])
-    .range([20, 100]);
+    .range([5, 100]);
 
 
   motivAll.push(d3.sum(ethni), d3.sum(reli), d3.sum(orien), d3.sum(disab), d3.sum(gend), d3.sum(identi), multi);
   //motivAll.push()
   // console.log(motivAll + ' aaa');
   // console.log(allEthni + ' aaa');
+allignDatasetforCondition2.push(motivAll)
+    console.log(allignDatasetforCondition2);
+
+
+
 
   circleoffset = 20;
   privousValue = 0;
@@ -235,21 +276,24 @@ function conditionTwo(inArray, moArray, svgPic) {
         .attr("opacity", .8)
         .attr("r", reIncidentsArray(d));
     })
+    .style("opacity", 0)
+    .attr("r", 0)
     .transition()
     .ease(d3.easeElastic.period(.7))
-    .duration(2000)
+    .duration(1000)
+    .style("opacity", .8)
     .delay(function(d, i) {
       return i * 25
     })
     //.style("opacity", .8)
     .attr("cy", function(d, i) {
       if (i > 0) {
-        privousValue = motivAll[i - 1];
+        privousValue = allignDatasetforCondition2[0][i - 1];
       }
-      circleoffset += reIncidentsArray(d) + reIncidentsArray(privousValue) + 30
+      circleoffset += reIncidentsArray(allignDatasetforCondition2[0][i]) + reIncidentsArray(privousValue) + 30
       return circleoffset;
     })
-    .attr("r", 0)
+
     .attr("r", function(d, i) {
       return reIncidentsArray(d)
     })
@@ -333,12 +377,17 @@ function conditionTwo(inArray, moArray, svgPic) {
 }
 //********************************************************************
 function conditionThree(inArray, svgPic) {
+
+
+
   this.inArray = inArray;
   this.svgPic = svgPic;
   let xP = 130;
   svgPic.selectAll("g").remove();
   svgPic.selectAll("text").remove();
   svgPic.selectAll("circle").remove();
+
+  d3.selectAll("svg").attr("height", heightVis);
 
   //remap the incidents data
   //var maxOfInci= incidentsArray.sort(d3.descending);
@@ -357,6 +406,9 @@ function conditionThree(inArray, svgPic) {
   circles.append("circle")
     //.classed('ctext',true)
     .attr("cx", xP)
+    // .attr("cy", function(d) {
+    //   return reIncidentsArray(d)
+    // })
     .attr("cy", function(d) {
       return reIncidentsArray(d)
     })
@@ -376,13 +428,25 @@ function conditionThree(inArray, svgPic) {
     .delay(function(d, i) {
       return i * 25
     })
-    //.style("opacity", .8)
+    .style("opacity", .8)
+    // .attr("cy", function(i, d) {
+    //   for(var a=0; a<incidentsArray3.length;a++){
+    //   if (a > 0) {
+    //     privousValue = incidentsArray3[a - 1];
+    //   }
+    //   circleoffset += reIncidentsArray(incidentsArray3[a]) + reIncidentsArray(privousValue) + 30
+    //       return circleoffset;
+    //   }
+    //
+    // })
     .attr("cy", function(d, i) {
       if (i > 0) {
-        privousValue = inArray[i - 1];
+        privousValue = allignDataset[i - 1];
       }
-      circleoffset += reIncidentsArray(d) + reIncidentsArray(privousValue) + 30
-      return circleoffset;
+      circleoffset += reIncidentsArray(allignDataset[i]) + reIncidentsArray(privousValue) + 30
+          return circleoffset;
+      //}
+
     })
     .attr("r", 0)
     // .transition()
@@ -429,9 +493,9 @@ function conditionThree(inArray, svgPic) {
     .attr('font-size', 12)
     .attr("y", function(d, i) {
       if (i > 0) {
-        privousValue = inArray[i - 1];
+        privousValue = allignDataset[i - 1];
       }
-      circleoffset += reIncidentsArray(d) + reIncidentsArray(privousValue) + 30;
+      circleoffset += reIncidentsArray(allignDataset[i]) + reIncidentsArray(privousValue) + 30;
       return circleoffset;
     })
     .style("opacity", 0)
@@ -455,9 +519,9 @@ function conditionThree(inArray, svgPic) {
     .classed('gtext', true)
     .attr("y", function(d, i) {
       if (i > 0) {
-        privousValue = inArray[i - 1];
+        privousValue = allignDataset[i - 1];
       }
-      circleoffset += reIncidentsArray(d) + reIncidentsArray(privousValue) + 30;
+      circleoffset += reIncidentsArray(allignDataset[i]) + reIncidentsArray(privousValue) + 30;
       return circleoffset + 15;
     })
     .attr("x", function(d) {
@@ -474,25 +538,37 @@ function conditionThree(inArray, svgPic) {
     .text(function(d, i) {
       return inArray[i]
     });
-}
+};
 
-
-svg.on("click", function() {
+d3.select("#views").on("click", function() {
   if (condition == 0) {
 
-    conditionTwo(incidentsArray, motivationArray, svg);
-    conditionTwo(incidentsArray2, motivationArray2, svg2);
-    conditionTwo(incidentsArray3, motivationArray3, svg3);
+    heightVis = 1000;
+
+    endingOne();
+
+    setTimeout(function() {
+      conditionTwo(incidentsArray, motivationArray, svg);
+      conditionTwo(incidentsArray2, motivationArray2, svg2);
+      conditionTwo(incidentsArray3, motivationArray3, svg3);
+    }, 500)
 
     condition = 1;
 
   } else if (condition == 1) {
+
+    heightVis = 1800;
     conditionThree(incidentsArray, svg);
     conditionThree(incidentsArray2, svg2);
     conditionThree(incidentsArray3, svg3);
 
     condition = 2;
+    d3.select("myfooter").style("bottom", 0);
   } else if (condition == 2) {
+    //loadingSvg();
+    heightVis = 400;
+
+    d3.select("myfooter").style("bottom", 0);
     conditionOne(incidentsArray, 130, svg);
     conditionOne(incidentsArray2, 130, svg2);
     conditionOne(incidentsArray3, 130, svg3);
@@ -501,3 +577,50 @@ svg.on("click", function() {
     //console.log(condition);
   };
 });
+
+function endingOne() {
+
+  // d3.selectAll("circle")
+  //     .style("opacity", 1)
+  //     .transition()
+  //     //.ease(d3.easeElastic.period(0.8))
+  //     .duration(2000)
+  //     .style("opacity", 0);
+
+  //  d3//.selectAll("g")
+  d3.selectAll("g")
+    .style("opacity", 1)
+    .transition()
+    .ease(d3.easeElastic.period(0.8))
+    .duration(500)
+    .style("opacity", 0);
+
+  // callback(incidentsArray, motivationArray, svg );
+
+}
+
+
+function scrollWin() {
+  window.scrollTo(0, 0);
+}
+
+window.onscroll = function() {
+  checkScrollPosition()
+};
+
+function checkScrollPosition() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    d3.select(".myfooter")
+      .style("opacity", 0)
+      .transition()
+      .duration(1000)
+      .style("opacity", 1);
+  } else {
+
+    d3.select(".myfooter")
+    .transition()
+    .duration(1000)
+    .style("opacity", 0);
+  }
+
+}
